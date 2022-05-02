c58473b16a97281df4c436a751c5b346cf49fc5d
Rhuan Gabriel <rhuan.santos@db1.com.br>
2022-05-02 18:45:04 -0300

    first day

---
+++ lib/modules/login/login_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/social_login/social_login_button.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: SizedBox(
+        width: size.width,
+        height: size.height,
+        child: Stack(
+          children: [
+            Container(
+              width: size.width,
+              height: size.height * 0.36,
+              color: AppColors.primary,
+            ),
+            Positioned(
+              top: 37,
+              left: 0,
+              right: 0,
+              child: Image.asset(
+                AppImages.person,
+                width: 208,
+                height: 300,
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Column(
+                children: [
+                  Image.asset(AppImages.logomini),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 30, left: 70, right: 70),
+                    child: Text(
+                      'Organize seus boletos em um só lugar',
+                      textAlign: TextAlign.center,
+                      style: TextStyles.titleHome,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(40),
+                    child: SocialLoginButton(
+                      onTap: () {
+                        print('Tapped');
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
